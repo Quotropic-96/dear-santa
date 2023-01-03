bf9e63bcbd0f46c0b9306051761bed0382ed428b
Gerard Solanes <gerardsolher@gmail.com>
2023-01-03 17:47:56 +0100

    Create data base for seed

---
+++ db/data.js
@@ -0,0 +1,39 @@
+const data = [
+    {
+        name: 'Lego Star Wars: Death Star',
+        image: 'https://i.ebayimg.com/images/g/Wb4AAOSwuPdifpxC/s-l640.jpg',
+        price: 850,
+        recipient: 'Ale',
+        description: 'This amazingly detailed battle station features an incredible array of minifigure-scale scenes, moving parts, characters and accessories from Episodes IV and VI.'
+    },
+    {
+        name:'Macbook Pro',
+        image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.macworld.com%2Fwp-content%2Fuploads%2F2021%2F07%2Fmacbook-air-2020-6.jpg%3Fquality%3D50%26strip%3Dall&f=1&nofb=1&ipt=6c09fc21b24c3c76a3b98fff9a0f8a6a56d3285c8ac27eaf902f61632d3b74fe&ipo=images',
+        price: 2500,
+        recipient: 'Gerard',
+        description: 'Amazing laptop to rule the Grid'
+    },
+    {
+        name: 'Nice Socks',
+        image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.s3DjvlBzGK8Bl5rc7gTTjgHaHa%26pid%3DApi&f=1&ipt=e399f2c49f9144b8d401811b6b3353ff4d0e2ef8591285f071b04dec2b95081f&ipo=images',
+        price: 10,
+        recipient: 'Dad',
+        description: 'Badass looking socks for dad'
+    },
+    {
+        name: 'Tea Pot',
+        image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.coffeeandtealovers.co.nz%2Fsite%2Ffile%2Fproduct%2F5970%2Fteapot-white.jpg&f=1&nofb=1&ipt=ef33172ca2cc3e2be426bd9b494e5313ffc459db447cee2916dbd9cec1a99f56&ipo=images',
+        price: 20,
+        recipient: 'Mum',
+        description: 'Traditional japanese tea pot'
+    },
+    {
+        name: 'Coffee machine',
+        image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi5.walmartimages.com%2Fasr%2Fe0289a41-9ab8-4e10-ae50-0fe117c1f46d_1.b8f40da5c2ee543727e1d3650f89ab37.jpeg&f=1&nofb=1&ipt=c1511ed57e9e9520ed10a60928231968670d5b3ce14b25433fa5da1d7e5cb029&ipo=images',
+        price: 200,
+        recipient: 'Sister',
+        description: `The world's most wonderful (and expensive) coffee machine`
+    }
+];
+
+module.exports = data;
