b588c86048a02cb901ae4e2e45cba03eae937f95
Gerard Solanes <gerardsolher@gmail.com>
2023-01-03 18:31:03 +0100

    Implement detail view

--- routes/presents.js
+++ routes/presents.js
@@ -12,4 +12,14 @@ router.get('/', async (req, res, next) => {
   }
 });
 
+router.get('/:presentId', async (req, res, next) => {
+  const { presentId } = req.params;
+  try {
+    const present = await Present.findById(presentId);
+    res.render('detail', present);
+  } catch (error) {
+    next(error);
+  }
+})
+
 module.exports = router;
