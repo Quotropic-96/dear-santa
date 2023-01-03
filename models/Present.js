1d599df477f60d887efb4e72498f48f761822be0
Gerard Solanes <gerardsolher@gmail.com>
2023-01-03 17:38:40 +0100

    Create schema for Present model

---
+++ models/Present.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const presentSchema = new Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        default: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fclipartmag.com%2Fimage%2Fpresent-drawing-21.png&f=1&nofb=1&ipt=91763ab281d67ea80405f4ce215ba131a5033a9f4ac291fa51d77ec953913a87&ipo=images'
+    },
+    price: {
+        type: Number,
+        min: 0
+    },
+    recipient: String,
+    description: String
+});
+
+const Present = mongoose.model('Present', presentSchema);
+module.exports = Present;
